586c3b0d7a88bfe0f79e3f438ee8bb342bcea6c8
Jonnii Ringo <jonathansamuels@Jonathans-MBP-2.lan>
2024-12-01 21:02:21 -0800

    Modified the appropriate file

--- src/index.jsx
+++ src/index.jsx
@@ -1,38 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-client"; 
+import App from "./components/App"; 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+root.render(<App />); 
 
-////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
 
-//////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+var numbers = [3, 56, 2, 48, 5];
 
-//Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+//Map -Create a new array by doing something with each item in an array.
+const newNumbers = numbers.map( x => x * 2);
+console.log("newNumbers");
 
-////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+//Filter - Create a new array by keeping the items that return true.
+const newNumbers1 = numbers.filter(num => num < 10);
 
-////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+// Reduce - Accumulate a value by doing something to each item in an array.
+var newNumber2 = numbers.reduce( (accumulator, currentNumber) => accumulator + currentNumber);
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+//Find - find the first item that matches from an array.
+const newNumber3 = numbers.find( num => num > 10);
+
+//FindIndex - find the index of the first item that matches.
+const newNumber4 = numbers.findIndex(num => num > 10);
+
+
